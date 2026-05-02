c19a485f94475c5d5d0e71e4d9e6f75aede65da3
dhruv bishnoi <bishnoishab502@gmail.com>
2026-05-02 15:04:58 +0530

    Add debug logs to server and client fetch
    Add console.log statements to aid debugging: log 'hello' in the server GET and POST handlers (index.js) and log the fetch Response object in the client testpost function (public/mypage.html). These additions are intended to help trace requests and responses during development.

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const port = 3000
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
+  console.log('hello')
   res.send('Hello World!')
 })
 
 
 app.post('/', (req, res) => {
+  console.log('hello')
   res.send('Data received!')
 })
 
