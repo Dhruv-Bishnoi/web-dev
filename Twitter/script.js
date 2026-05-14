fe92db1ffa8fcf9b26ebbfce49104261c3285679
dhruv bishnoi <bishnoishab502@gmail.com>
2026-05-14 21:19:55 +0530

    Update script.js

--- Twitter/script.js
+++ Twitter/script.js
@@ -1,7 +1,7 @@
 const multer = require("multer")
 const express = require("express")
 const path = require("path")
-
+``  
 const app = express()
 
 app.use(express.static("public"))
@@ -26,6 +26,7 @@ const upload = multer({ storage })
 
 
 // IMAGE UPLOAD
+
 app.post("/upload", upload.single("image"), (req, res) => {
 
     res.json({
