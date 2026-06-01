589b01e3bd2c2d489453738b41ad9e5252e5dd42
dhruv bishnoi <bishnoishab502@gmail.com>
2026-06-01 21:52:35 +0530

    Components and props

---
+++ react-dev/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>gome</li>
+        <li>home</li>
+        <li>how</li>
+      </ul>
+    </div>
+  )
+}
+
+export default navbar
