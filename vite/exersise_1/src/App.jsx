34c84136a4fc77924533fbcd7b55a7a07c0674fd
dhruv bishnoi <bishnoishab502@gmail.com>
2026-06-11 23:00:06 +0530

    Add Vite React exercise template
    Create a new Vite + React starter project at vite/exersise_1. Adds index.html, package.json, package-lock.json (renamed), vite.config.js, ESLint config, README, public assets (favicon/icons), src files (App.jsx demo fetching todos from jsonplaceholder, main.jsx, styles, assets) and supporting images/SVGs. Also includes a .gitignore and minor update to vite/vite-project/src/App.jsx. This provides a minimal runnable example and dev scripts to start the project.

---
+++ vite/exersise_1/src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [todo, setTodo] = useState([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    fetch(          'https://jsonplaceholder.typicode.com/todos'
+)
+      .then((response) => response.json())
+      .then((data) => {
+        setTodo(data)
+      })
+      .catch((err) => {
+        setError(err.message)
+      })
+  }, [])
+
+
+
+  return (
+
+      
+    
+    <>
+      <h1>Todo</h1>
+      {todo.map(post=>(
+          
+                  <div
+          key={post.id}
+          style={{
+            border: "1px solid white",
+            margin: "10px",
+            padding: "10px"
+          }}
+        >
+
+<p><strong>Title:</strong> {post.title}</p>
+<p><strong>Completed:</strong> {post.completed ? 'Yes' : 'No'}</p>
+<p><strong>todo ID:</strong> {post.id}</p>
+        </div>
+
+
+      ))}
+      
+    </>
+  )
+}
+
+export default App
--- vite/vite-project/src/App.jsx
+++ vite/vite-project/src/App.jsx
@@ -1,64 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
-
-  const [todos, settodos] = useState([
-    {
-      title:"hey",
-      disc:"hey how are you first"
-    },
-    {
-      title:"hello",
-      disc:"hey how are you sec"
-    },
-    {
-      title:"huhuhuh",
-      disc:"hey how are you third"
-    },
-  ])
-function App() {
-  const [count, setCount] = useState(0)
-
-  const [todos, settodos] = useState([
-    {
-      title:"hey",
-      disc:"hey how are you first"
-    },
-    {
-      title:"hello",
-      disc:"hey how are you sec"
-    },
-    {
-      title:"huhuhuh",
-      disc:"hey how are you third"
-    },
-  ])
+  useEffect(()=>{
+    const a =  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    console.log(a)
+   
+  }, [])
+  
 
 
-
-  const Todo = ({todos})=>{
- return (
-    <>
-<div className=" border-2 m-8  border bg-danger border-white">
-    <div className="Todos text-3xl font-bold underline">{todos.title}</div>
-    <div className="Todos">{todos.disc}</div>
-    </div>
-    </>)
-  } 
-
-  return (
+return (
     <>
 
-    {todos.map(todo=>{
-      return <Todo todos={todo}/> 
-    })}
-
-
-
+hello
 
     </>
   )
